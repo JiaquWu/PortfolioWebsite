28c256c78290f545ef1dda75483b9cfb132d0693
Wu <wujiaqu1233@gmail.com>
2022-11-02 14:25:39 +0100

    add some notes for code snippets

--- src/components/CodeSnippets/CodeSnippets.js
+++ src/components/CodeSnippets/CodeSnippets.js
@@ -44,7 +44,8 @@ function CodeSnippets() {
             <Card.Img src={ability_02} style = {{paddingBottom:2}}></Card.Img>
             <Card.Img src={ability} style = {{paddingBottom:2}}></Card.Img>
             <Card.Text style={{ textAlign: "left" }}> 
-            ability and buff system
+            This is the infrastructure for the ability and buffs system in the Auto Battler project. The acquisition of custom buffs and the various functions associated with buffs are implemented through the abstract factory pattern and ScriptableObject. 
+            Buffs are part of the trait and ability system in this project, while other abilities are implemented by passing data and methods to custom triggers.
             </Card.Text>
         </Card>
         </Col>
@@ -58,7 +59,8 @@ function CodeSnippets() {
             <Card.Img src={event_05} style = {{paddingBottom:2}}></Card.Img>
             <Card.Img src={event_06} style = {{paddingBottom:2}}></Card.Img>
             <Card.Text style={{ textAlign: "left" }}> 
-            game event system
+            An example of a global event system with a champion parameter, where the champion triggers the corresponding event by calling the static method TriggerEvent in the singleton script, and other objects that need to be notified can subscribe to the event to trigger other functions. 
+            The event system in the Auto Battler project is implemented with events of different parameters and reduces code coupling.
             </Card.Text>
         </Card>
         <Card className="project-codesnippet-card-view">
@@ -67,14 +69,18 @@ function CodeSnippets() {
             <Card.Img src={path_02} style = {{paddingBottom:2}}></Card.Img>
             <Card.Img src={path_03} style = {{paddingBottom:2}}></Card.Img>
             <Card.Text style={{ textAlign: "left" }}> 
-            dynamic path finding system
+            The basic logic of the champion pathfinding system in the Auto Battler project. 
+            When a champion moves around the map after a route has been found, it marks the next target quad it moves to as non-walkable and marks the current quad as walkable. This way, when other champions request a route from the A* pathfinding algorithm, the algorithm will recognise that the next target quad of other champions is unwalkable, 
+            thus avoiding overlap between the routes of different champions.
             </Card.Text>
         </Card>
         <Card className="project-codesnippet-card-view">
             <Card.Title>finite state machine </Card.Title>
             <Card.Img src={statemachine} style = {{paddingBottom:2}}></Card.Img>
             <Card.Text style={{ textAlign: "left" }}> 
-            finite state machine 
+            Code snippet of the champion finite state machine from the Auto Battler project. 
+            The project splits the logic of the champion's behaviour by creating instances of the different states of the champion, 
+            and customising the transition conditions between the different state machines makes it easy to switch between event-based champion states.
             </Card.Text>
         </Card>
         </Col>
@@ -87,7 +93,8 @@ function CodeSnippets() {
             <Card.Img src={command_02} style = {{paddingBottom:2}}></Card.Img>
             <Card.Img src={command_03} style = {{paddingBottom:2}}></Card.Img>
             <Card.Text style={{ textAlign: "left" }}> 
-            undo system
+            Implementation of the undo feature based on command pattern in Buddy games. When the player performs an action in the game, the action is not implemented directly, but the action itself and undo action are stored in a command, which is stored in a stack. When the player performs an undo action, 
+            it simply pops the command from the stack and executes the undo delegate, and the game's undo feature is implemented.
             </Card.Text>
         </Card>
 
@@ -98,7 +105,9 @@ function CodeSnippets() {
             <Card.Img src={gameplaytag} style = {{paddingBottom:2}}></Card.Img>
             <Card.Img src={action} style = {{paddingBottom:2}}></Card.Img>
             <Card.Text style={{ textAlign: "left" }}> 
-            action system
+            Implementation of a GameplayTags based action system in a third person shooter project developed in C++ and blueprints in Unreal5. 
+            The game's character abilites and buffs are specifically implemented in blueprints that inherits from the Action class. Abilites and buffs that have a duration are given a specific GameplayTag, which is removed at the end of the duration or other conditions. Other systems in the game recognise these tags and react to them, 
+            for example if the player has a buff that they are currently immune, they will be skipped when others are dealing damage to them.
             </Card.Text>
         </Card>
         

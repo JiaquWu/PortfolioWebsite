53f289c09f7d102e939f095903ae74a2a42d5dc8
JiaquWu <wujiaqu1233@gmail.com>
2022-12-18 23:35:39 +0100

    add 2 new cards, add finish time for projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,7 +29,9 @@ function Projects() {
               videoWidth = "1280"
               // imgPath={chatify}
               title="Receiver 2"
-              description = <h6 style={{lineHeight:1.5}}>My contribution: <br></br>· Implemented new game features, and resolved 20+ miscellaneous game bugs/issues, most of which can be found in this 
+              description = <h6 style={{lineHeight:1.5}}>
+                <i className="purple">Apr 2021 - Jun 2021</i><br></br>
+                My contribution: <br></br>· Implemented new game features, and resolved 20+ miscellaneous game bugs/issues, most of which can be found in this 
               <a href="https://store.steampowered.com/news/app/1129310/view/3019092467758679460"> game update information </a>.
               <br></br>· Worked with other developers to develop new weapons and new game maps.</h6>  
             />
@@ -43,6 +45,7 @@ function Projects() {
               videoWidth = "100%"
               title="Jump!"
               description=<h6 style={{lineHeight:1.5}}>
+              <i className="purple">Sep 2020 - Dec 2020</i><br></br>
               Jump! is a collaborated work with the University of Skövde, WWF, and SLU with the aim of increasing interest towards the seabirds living in the Baltic Sea. 
               <br></br>
               <br></br>My contribution: <br></br>· Implemented most of game features with C# and Unity.
@@ -63,6 +66,7 @@ function Projects() {
               videoWidth = "100%"
               title="Auto-Battler Project"
               description=<h6 style={{lineHeight:1.5}}>
+              <i className="purple">Jul 2022 - Sep 2022</i><br></br>
               It's a game project that I have been working on alone recently which contains basic features of the auto-battler game genre in single player game mode with Unity, including some of the more complex but interesting game systems:
               <br></br>
               <br></br>· Utilized A-star algorithm to implement a dynamic pathfinding system for the champion during combat, and a finite state machine was used to implement changes in the champion's behaviour in different states, which together implemented the champion's AI system.
@@ -83,6 +87,7 @@ function Projects() {
               videoWidth = "100%"
               title="Online Project"
               description=<h6 style={{lineHeight:1.5}}>
+              <i className="purple">Nov 2021 - Dec 2021</i><br></br>
               This project was an attempt by me and a few other developers to develop a Steam online multiplayer demo using Unreal4 at a Game Jam and ended up with a mini game that could be played by multiple players through Steam network.
               <br></br>
               <br></br>My contribution: 
@@ -96,12 +101,55 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard  
+              title="FPS Project"
+              videoTitle = "FPS Project"
+              videoLink = "https://www.youtube.com/embed/MJjFWTQX8S4"
+              videoHeight = "213"
+              videoWidth = "100%"
+              description=<h6 style={{lineHeight:1.5}}>
+              <i className="purple">Oct 2022</i><br></br>
+              A first person shooter game prototype built with Unreal 5 and C++ in 2 weeks.
+              <br></br>
+              <br></br>Main game features:
+              <br></br>· Implemented action component and attribute componenent for game ability system and character attributes.
+              <br></br>· Utilized EQS and behaviour tree to created AI system.
+              </h6>
+              //ghLink="https://github.com/soumyajit4419/Plant_AI"
+              //demoLink="https://jiaqu-wu.itch.io/procrastinationsg"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard 
+              videoTitle = "IceSokoban"
+              videoLink = "https://www.youtube.com/embed/lLLQDieJXjc"
+              videoHeight = "213"
+              videoWidth = "100%"
+              title="An Ice-Sokoban puzzle game"
+              description=<h6 style={{lineHeight:1.5}}>
+              <i className="purple">Dec 2022</i><br></br>
+              <br></br>A 3d top-down Sokoban puzzle game built with Unity in a week.
+              <br></br>
+              <br></br>Main game features: 
+              <br></br>· Implemented a A Monster's Expedition-like character controller.
+              </h6> 
+              
+              ghLink="https://github.com/JiaquWu/IceSokoban"
+              demoLink="https://jiaqu-wu.itch.io/icesokoban"// <--------Please include a demo link here
+              isGithub = "true"
+            />
+          </Col>
+
+          
+          <Col md={4} className="project-card">
+            <ProjectCard  
               title="Cat Clinic"
               videoTitle = "Cat Clinic"
               videoLink = "https://www.youtube.com/embed/hjEBOPoB3uk"
               videoHeight = "213"
               videoWidth = "100%"
               description=<h6 style={{lineHeight:1.5}}>
+              <i className="purple">Mar 2021 - May 2021</i><br></br>
               This is a serious game prototype that I worked on with a friend for his thesis, it's designed to help procrastinators reduce academic procrastination. I built it with his artwork and design draft.
               <br></br>
               <br></br>Main game features:
@@ -112,7 +160,6 @@ function Projects() {
               demoLink="https://jiaqu-wu.itch.io/procrastinationsg"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard 
               videoTitle = "Buddy"
@@ -120,7 +167,9 @@ function Projects() {
               videoHeight = "213"
               videoWidth = "100%"
               title="Buddy"
-              description=<h6 style={{lineHeight:1.5}}>This game project is my thesis work for my Master's degree at the University of Skövde, and I finished the whole project and <a href="http://www.diva-portal.org/smash/record.jsf?dswid=4471&pid=diva2%3A1676937&c=1&searchType=SIMPLE&language=en&query=jiaqu&af=%5B%5D&aq=%5B%5B%5D%5D&aq2=%5B%5B%5D%5D&aqe=%5B%5D&noOfRows=50&sortOrder=author_sort_asc&sortOrder2=title_sort_asc&onlyFullText=false&sf=all">thesis</a> by myself, excluding the artwork. 
+              description=<h6 style={{lineHeight:1.5}}>
+                <i className="purple">Feb 2022 - May 2022</i><br></br>
+                This game project is my thesis work for my Master's degree at the University of Skövde, and I finished the whole project and <a href="http://www.diva-portal.org/smash/record.jsf?dswid=4471&pid=diva2%3A1676937&c=1&searchType=SIMPLE&language=en&query=jiaqu&af=%5B%5D&aq=%5B%5B%5D%5D&aq2=%5B%5B%5D%5D&aqe=%5B%5D&noOfRows=50&sortOrder=author_sort_asc&sortOrder2=title_sort_asc&onlyFullText=false&sf=all">thesis</a> by myself, excluding the artwork. 
               <br></br>The game was originally designed to see if the player could understand the rules and get the "every life counts" message that I wanted to send to the player without providing explicit explanation of the rules to the player. Overall I spent more effort on the game design than on the programming.
               <br></br>
               <br></br>Main game features: 
@@ -132,7 +181,6 @@ function Projects() {
               isGithub = "true"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               videoTitle = "Call of Wind"
@@ -141,6 +189,7 @@ function Projects() {
               videoWidth = "100%"
               title="Call of Wind"
               description=<h6 style={{lineHeight:1.5}}>
+              <i className="purple">Jul 2019 - Sep 2019</i><br></br>
               Call of Wind is a 2d puzzle platformer game that I worked on with other student developers for around two months in 2019, and it won a prize in a student game development competition in China at the time.
               <br></br>
               <br></br>My contribution:

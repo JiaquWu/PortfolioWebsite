5fd54dbb2ecc597a20f12f9da28e7b6fdba9984b
JiaquWu <wujiaqu1233@gmail.com>
2025-05-09 02:07:02 +0200

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/jiaqu_resume_250507.pdf";
+import pdf from "../../Assets/../Assets/jiaqu_resume_250509.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 //import { pdfjs } from "react-pdf";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -10,7 +10,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/JiaquWu/PortfolioWebsite/master/src/Assets/jiaqu_resume_250507.pdf";
+  "https://raw.githubusercontent.com/JiaquWu/PortfolioWebsite/master/src/Assets/jiaqu_resume_250509.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
